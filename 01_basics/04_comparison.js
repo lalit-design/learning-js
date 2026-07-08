058c75788f49856bad4b1aef6161656c414d8e1f
lalit kumar <lalitup70055@gmail.com>
2026-07-08 17:46:36 +0000

    Add comparison and datatypes summary basics

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log("2">1);
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null >=0);
+// console.log(null<=0);
+// when we compare the null to <>= or any equality
+// the compiler treats null as 0 and thenn compares 
+// it
+// console.log(underfined == 0);
+// undefined will always give false with any equality
+// triple ===
+console.log("2"===2);
+
+
+
+
