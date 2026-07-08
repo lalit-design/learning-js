b8198c1b4765042e27535d8ddea1c15997a5a54a
lalit kumar <lalitup70055@gmail.com>
2026-07-08 18:22:28 +0000

    updated about datatypes

--- 01_basics/datetypes_summary.js
+++ 01_basics/datetypes_summary.js
@@ -3,3 +3,19 @@
 // BigInt
 // Reference (Non primitive) no copy
 // array,objects,functions 
+const heroes = ["hanuman"+"shatiman"+"batman"];
+let objects = {
+    name : "lalit",
+    age:22
+}
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+console.log(typeof(heroes));
+console.log(typeof objects);
+console.log(typeof myFunction);
+
+//both array , object are of object type
+//function is of type function  
+
