4b3545ab1b293e5f996aca50e1d9480ae99af14a
lalit kumar <lalitup70055@gmail.com>
2026-07-10 11:09:10 +0000

    nums and math

---
+++ 01_basics/06_numsandmath.js
@@ -0,0 +1,24 @@
+// const score = 23
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+////////////////////////////maths///////
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*10)+(max-min));
+
