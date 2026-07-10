60ad28ecf050caecb12656cbba84b4f1ef17a118
lalit kumar <lalitup70055@gmail.com>
2026-07-10 11:01:17 +0000

    about strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,24 @@
+// const name = "lalit"
+// const repoCount = 23
+// console.log(name+repoCount);
+// console.log(`hello my name is ${name} and gitrepocount is ${repoCount}`);
+// const gameName = new String('lalit')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+const newName = "  kaushik  "
+console.log(newName.substring(0,4));
+console.log(newName);
+console.log(newName.trim());
+
+
+
+
+
+
+
+
