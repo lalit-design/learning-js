dff0bbba4080a07eb3c06823b9b85ff12d322f00
lalit kumar <lalitup70055@gmail.com>
2026-07-13 10:12:29 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,24 @@
+// let a = 300
+// if(true){
+//     let a = 10
+//     console.log("Inner",a);
+// }
+// console.log(a);
+
+// function one() {
+//    let name = "laid";
+
+//     function two() {
+//         console.log(name);
+        
+//     }
+//     two();
+// }
+// one();
+
+//++++++++++++++++++ interesting ++++++++++
+
+addtwo(5)
+const addTwo = function(num){
+    return num + 2
+}
